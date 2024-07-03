9983934f71831c7eeaadfc5e859adf83af121fac
Rejuan7 <146606152+Rejuan7@users.noreply.github.com>
2024-07-04 01:08:33 +0600

    fix a little

--- src/Components/Education/Education.jsx
+++ src/Components/Education/Education.jsx
@@ -46,7 +46,7 @@ const Education = () => {
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="white" className="h-5 w-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
     </div>
     <div className="timeline-start text-white md:text-end mb-10">
-      <time className="font-mono italic">2017-2018</time>
+      <time className="font-mono italic">2016-2018</time>
       <div className="text-lg font-black">Sristy Academic School Tangail</div>
       <div>
         <h1 className="mb-3">Secondary School Certificate</h1>
